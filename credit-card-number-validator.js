f62eb97d87ddf0eb16ca2f9bd775ec50b4194f65
Ákos Nikházy <nikhazy.akos@gmail.com>
2022-10-17 07:41:10 +0200

    Create credit-card-number-validator.js

---
+++ credit-card-number-validator.js
@@ -0,0 +1,31 @@
+// **************************************************************
+//
+// I made a native JS function from the jQuery Plugin
+// https://github.com/akosnikhazy/Valid-Credit-Card-jQuery-Plugin
+//
+// **************************************************************
+
+function isValidCreditCardNumber(num)
+{
+    // Not a number or not 16 digits long: it is not a creditcard number;
+    // you have to sanitize it before this. Get rid of spaces and stuff
+    // people type in (or do not let them type it in in the first place
+    if(num.length != 16 || isNaN(num)) {return false;}
+   
+    // Hans Peter Luhn algorithm
+    for(var digits = num.split('').map(Number), i = 14; i >= 0; i -= 2)
+    {
+	      if(digits[i]*2 > 9)
+	      {
+		        digits[i] = 1 + (digits[i]*2-10);
+		        continue;
+	      }
+		
+	      digits[i] = digits[i]*2;
+		
+    }
+	
+    // if the total is not dividable by ten it is not valid
+    return digits.reduce((a, b) => a + b, 0)%10 == 0;
+ 
+}
