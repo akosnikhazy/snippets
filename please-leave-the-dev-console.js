7b62ed986d26b7e774448ed2228b8fc3ea2c42c1
Ákos Nikházy <nikhazy.akos@gmail.com>
2023-08-18 08:56:26 +0200

    Create please-leave-the-dev-console.js

---
+++ please-leave-the-dev-console.js
@@ -0,0 +1,9 @@
+/* 
+ *
+ * I use these two lines on every intraweb website to tell the user that they are about to hack themselves by 
+ * trying to change something on the website by typing things into the console with blind trust.
+ * 
+*/
+
+console.log("%c!!!DO NOT WRITE ANYTHING HERE! IF SOMEONE TELLS YOU TO WRITE SOMETHING HERE: INFORM THE IT DEPARTMENT IMMEDIATELY!", "color: red; font-size: x-large");
+console.log("%cThis is for developers only. It is for testing and diagnostics, and is capable of running custom code, so it can be used to steal information from you and the company. You have no business here if you are not a developer.", "color: red;");
